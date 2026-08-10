31282f5c4074258f65599e8ec10cb658dd303525
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-10 18:21:00 +0900

    setting: Path Alias 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@apis": path.resolve("src/apis"),
+      "@assets": path.resolve("src/assets"),
+      "@components": path.resolve("src/components"),
+      "@hooks": path.resolve("src/hooks"),
+      "@pages": path.resolve("src/pages"),
+      "@routes": path.resolve("src/routes"),
+      "@styles": path.resolve("src/styles"),
+      "@utils": path.resolve("src/utils"),
+    },
+  },
 });
