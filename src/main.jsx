0d356934094464f93ea918dddbdb8d1dbd332ff7
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-10 19:03:40 +0900

    style: 전역 스타일 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
-import "./index.css";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
