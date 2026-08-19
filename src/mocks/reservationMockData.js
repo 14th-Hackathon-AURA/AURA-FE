62ad50905f57cee68b4e6e6aa94c0aaaa8fb72a9
lyeonj <lyeonj@sookmyung.ac.kr>
2026-08-20 04:46:13 +0900

    feat: 매장 예약 폼 컴포넌트 구현

---
+++ src/mocks/reservationMockData.js
@@ -0,0 +1,25 @@
+export const RESERVATION_PRODUCT_OPTIONS = [
+  "토트백 & 쇼퍼백",
+  "숄더백 & 크로스백",
+  "백팩",
+  "벨트백",
+  "탑 핸들백",
+  "미니백",
+  "클러치 & 파우치",
+  "트래블",
+  "스트랩 & 액세서리",
+];
+
+export const RESERVATION_CONSULT_OPTIONS = [
+  "오염(화장품, 음식물, 잉크 등)",
+  "스크래치",
+  "물 얼룩·빗물 자국",
+  "이염(청바지, 의류 등)",
+  "이염 (청바지, 옷)",
+  "가죽 갈라짐·건조",
+  "곰팡이",
+  "변색",
+  "악취",
+];
+
+export const MOCK_SELECTED_STORE = "MCM 롯데백화점 본점";
